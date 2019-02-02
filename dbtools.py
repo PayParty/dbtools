5386f5d02f362a93c4cc76ccd7d2ca631a7915df
João Moraes <jl.lima.moraes@gmail.com>
2019-02-01 23:05:52 -0200

    removed redundant documentation from dbtools.py

--- dbtools.py
+++ dbtools.py
@@ -5,12 +5,6 @@
 # author: João 'Jam' Moraes
 # license: MIT
 #
-# This tool is designed for identifying inconsistencies in MongoDB databases
-# based on pre-defined models. It supports automated analysis over multiple
-# databases located on multiple servers as a single batch operation. It is
-# structured with memory efficiency as a priority, requiring the minimum amount
-# of memory possible for each operation.
-#
 
 import src as DBTool
 import json
